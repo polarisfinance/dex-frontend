8c6a2db366af5ce96c2f48a353816b0a69282c0d
Slicker1892 <slicker.slicker@yahoo.com>
2022-10-20 21:41:27 +0100

    bug

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -174,9 +174,9 @@ export default defineComponent({
       const tokenList = [] as string[];
 
       for (const token of Object.entries(tokens)) {
-        console.log(token);
-        const tokenName: string = token[1]['name'];
-        const tokenAddress: string = token[1]['address'];
+        console.log(token[1][1]);
+        const tokenName: string = token[1][1]['name'];
+        const tokenAddress: string = token[1][1]['address'];
 
         if (
           tokenName &&
