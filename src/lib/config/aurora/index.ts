e30cb1c3e5e2a47bd4115c98bc3bd427f7e53ab6
eYdr1en <54525514+eYdr1en@users.noreply.github.com>
2023-05-27 00:17:48 +0200

    fix wss link aurora

--- src/lib/config/aurora/index.ts
+++ src/lib/config/aurora/index.ts
@@ -18,7 +18,7 @@ const config: Config = {
   visibleInUI: true,
   testNetwork: false,
   rpc: `https://aurora-mainnet.infura.io/v3/${keys.infura}`,
-  ws: `https://aurora-mainnet.infura.io/v3/${keys.infura}`,
+  ws: `wss://aurora-mainnet.infura.io/v3/${keys.infura}`,
   explorer: 'https://explorer.aurora.dev',
   explorerName: 'Aurora Explorer',
   subgraph:
